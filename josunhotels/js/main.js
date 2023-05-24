1a000f5ab013ef2bc39fba4ad36cb04487374ce0
jiwo <enwns2000@naver.com>
2023-05-24 17:26:21 +0900

    first commit

--- josunhotels/js/main.js
+++ josunhotels/js/main.js
@@ -16,4 +16,19 @@ $(function () {
       $(this).stop().slideUp();
     }
   );
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+  });
+
+  $(".custom1").owlCarousel({
+    animateOut: "slideOutDown",
+    animateIn: "flipInX",
+    items: 1,
+    margin: 30,
+    stagePadding: 30,
+    smartSpeed: 450,
+    animateOut: "fadeOut",
+  });
 });
